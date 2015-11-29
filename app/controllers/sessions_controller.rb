8d50103632fe96a26f3f31ce09d3d1b840197ad9
Paul Millar <paul@digitaldawn.com.au>
2015-11-29 12:01:21 +1100

    Ensured users can't login unless approved

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,17 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # This is pants code, but i'll refactor later
   def create
-    if @user = login(params[:user][:email], params[:user][:password])
-      flash[:notice] = "Login Successful"
-      redirect_to directory_index_path
+    if @user = User.find_by_email(params[:user][:email])
+      if @user.valid_password?(params[:user][:password]) && @user.accepted?
+        login(params[:user][:email], params[:user][:password])
+        flash[:notice] = "Login Successful"
+        redirect_to directory_index_path
+      else
+        flash[:notice] = "You're application is pending approval"
+        redirect_to root_path
+      end
     else
       flash.now[:alert] = 'Login failed'
       render :new
